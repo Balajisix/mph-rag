6069cd9316dff096a69400df8d52374469a1318c
Balaji V4 <balaji.v4@mphasis.com>
2026-01-19 13:12:10 +0530

    removed unused imports

--- rag_without_vector_db.py
+++ rag_without_vector_db.py
@@ -7,7 +7,6 @@ from dotenv import load_dotenv
 from langchain_huggingface import HuggingFaceEmbeddings, HuggingFaceEndpoint, ChatHuggingFace
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
-from langchain_core.prompts import ChatPromptTemplate
 
 app = FastAPI(title="Without Vector DB")
 load_dotenv()
