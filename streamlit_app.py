fa42cdd2f5e1bc471688b39a4930d53f9511317c
Balaji V4 <balaji.v4@mphasis.com>
2026-01-19 17:48:22 +0530

    url change

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
  
-API_URL = "http://localhost:8000"
+API_URL = "https://mph-rag-backend.onrender.com"
  
 st.set_page_config(page_title="FAISS RAG Chatbot", page_icon="🤖")
 st.title("RAG Assignment")
